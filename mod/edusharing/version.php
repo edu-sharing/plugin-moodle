0fa14bd51448c27fd026eee9876525551d2065d5
shippeli <hippeli@edu-sharing.com>
2018-10-17 11:51:53 +0200

    add meta information

--- mod/edusharing/version.php
+++ mod/edusharing/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_edusharing';
-$plugin->version = 2018100901;
+$plugin->version = 2018101701;
 $plugin->requires = 2015051100;
 $plugin->cron = 0;
-$plugin->release = 'v4.1';
+$plugin->release = 'v4.2';
