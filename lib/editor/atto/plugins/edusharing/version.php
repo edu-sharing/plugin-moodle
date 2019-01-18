8b8995d94757ecaecf2098dd71da40e9adf48175
shippeli <hippeli@edu-sharing.com>
2019-01-18 10:55:24 +0100

    atto dialog add help gif

--- lib/editor/atto/plugins/edusharing/version.php
+++ lib/editor/atto/plugins/edusharing/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019011001;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019011801;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_edusharing';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_BETA;
