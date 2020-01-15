d97f7a24ae78c520992729cdbf06858df39a62b4
chr-ernst <ernst@edu-sharing.net>
2020-01-15 13:31:35 +0100

    fix: url parameter encoding for atto editor;

--- lib/editor/atto/plugins/edusharing/version.php
+++ lib/editor/atto/plugins/edusharing/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019061101;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2020011504;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_edusharing';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
