c439fa5a63efa5f2d93394bb9ccd7d819a3ee2c1
chr-ernst <ernst@edu-sharing.net>
2020-01-30 17:23:41 +0100

    fix: improved the filter popin animation;

--- filter/edusharing/version.php
+++ filter/edusharing/version.php
@@ -27,7 +27,7 @@ $plugin->component = 'filter_edusharing';
 $plugin->dependencies = array(
     'mod_edusharing'  => 2018072701,
 );
-$plugin->version = 2020012301; //YYYYMMDDXX
+$plugin->version = 2020013006; //YYYYMMDDXX
 $plugin->requires = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v5.1';
